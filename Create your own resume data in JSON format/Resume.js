8fd4222ed4496194b755cb54f3b72f71f9c70ebb
Ganeshmahi47 <108626667+Ganeshmahi47@users.noreply.github.com>
2022-08-16 00:36:58 +0530

    Add files via upload

---
+++ Create your own resume data in JSON format/Resume.js
@@ -0,0 +1,50 @@
+let myResume={
+    "RESUME": {
+      "name": "Ganesh s",
+      "email": "shagne468@gmail.com",
+      "phone": 9943674047,
+      "degree": "B.E",
+      "address": {
+        "Landmark": "9/27 vananthittu theroor",
+        "postalCode": 629704,
+        "city": "nagercoil",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+   
+    },
+    "work": [
+      {
+        "company": "Teame lease",
+        "position": "system adminstrator",
+        "startDate": "2021-8-07",
+        
+      },
+    ],
+    "education": [
+      {
+        "institution": "ponjesly College Of Engineering",
+        "department": "E.C.E",
+        "studyType": "fulltime",
+        "Passes out": 2021,
+        "gpa": 7.5,
+      }
+    ],
+    "skills": [
+      {
+        "Known Progarming language": "python,javascript",
+        "level": "beginer",}
+       
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Darwing,programming",
+      }
+    ]
+  }
+  console.log(myResume);
